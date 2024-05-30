dbef1de595b777aec299fe3dbcd8139b52d70625
JACORVI <117043793+JACORVI@users.noreply.github.com>
2024-05-29 19:30:25 -0500

    inicio
    1er commit

---
+++ src/main/java/com/example/gtics_lab7_20181526/GticsLab720181526Application.java
@@ -0,0 +1,13 @@
+package com.example.gtics_lab7_20181526;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab720181526Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GticsLab720181526Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gtics_lab7_20181526/GticsLab720181526ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gtics_lab7_20181526;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab720181526ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
